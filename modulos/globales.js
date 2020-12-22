f38949ebc28f51e911431a87cb196dc9653fe5ba
jesusGGZ <jesusgarcia@morpheusdss.com>
2020-12-22 14:59:50 -0600

    Manejo de modulo globales, introduccion

---
+++ modulos/globales.js
@@ -0,0 +1,22 @@
+/* let i = 0; 
+let intervalo = setInterval(function() {
+    console.log('Hola');
+    if (i === 3) {
+        clearInterval(intervalo);
+    }
+    i++;
+}, 1000); */
+
+/* setImmediate(function () {
+   console.log('Hola'); 
+}); */
+
+/* console.log(process); */
+
+/* console.log(__dirname); */
+
+/* console.log(__filename); */
+
+globalThis.miVariable = 'elValor';
+
+console.log(miVariable);
